ca8ea382b297b0b9ffdda30a3650a7b94e9e5fd0
Alvin1284 <81688085+Alvin1284@users.noreply.github.com>
2022-06-08 14:50:59 +0300

    Testing

---
+++ hekima/src/main/java/io/web/TransactionController.java
@@ -0,0 +1,26 @@
+package web;
+
+import entity.Transaction;
+import query.TransactionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+public class TransactionController {
+
+    public TransactionController(final TransactionService transactionService){
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/{accountNumber}")
+    public List<Transaction> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber){
+        return transactionService.findAllByAccountNumber(accountNumber);
+    }
+
+    private final TransactionService transactionService;
+}
